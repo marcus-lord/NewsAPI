e8e425635389bf2d0d850dfa9dfaf3641707cc7d
Marcus Lord <marcus.f.lord@gmail.com>
2024-12-09 14:16:36 -0500

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Marcus Lord (student LM) on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
